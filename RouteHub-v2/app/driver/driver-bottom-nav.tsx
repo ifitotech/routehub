21065854833cac6d55a7d812dd41375ccd37a0d3
ifitotech <rodolfito76@gmail.com>
2026-08-29 12:53:27 -0400

    Fix Driver navigation production build

--- RouteHub-v2/app/driver/driver-bottom-nav.tsx
+++ RouteHub-v2/app/driver/driver-bottom-nav.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link'
 'use client'
+
+import Link from 'next/link'
 
 import {Home, List, MapPin, MoreHorizontal} from 'lucide-react'
 import {usePathname,useSearchParams} from 'next/navigation'
