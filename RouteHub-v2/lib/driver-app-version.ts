1f264583932f9c6ff6a23aedcd59b1608b087ea8
ifitotech <rodolfito76@gmail.com>
2026-09-02 06:31:43 -0400

    Fix driver settings preferences layout

--- RouteHub-v2/lib/driver-app-version.ts
+++ RouteHub-v2/lib/driver-app-version.ts
@@ -1,4 +1,4 @@
 export const DRIVER_DAY = '0.4'
 // Increment for each published change during the current beta day.
-export const DRIVER_BETA = 4
+export const DRIVER_BETA = 5
 export const DRIVER_APP_VERSION = `${DRIVER_DAY}beta${DRIVER_BETA}`
