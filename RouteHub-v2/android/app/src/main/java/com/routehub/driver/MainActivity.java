4d62294590e855a52852661b5f1b94caebdd7c81
ifitotech <rodolfito76@gmail.com>
2026-09-16 08:04:45 -0400

    Apply native Android insets to Driver chrome

--- RouteHub-v2/android/app/src/main/java/com/routehub/driver/MainActivity.java
+++ RouteHub-v2/android/app/src/main/java/com/routehub/driver/MainActivity.java
@@ -1,11 +1,25 @@
 package com.routehub.driver;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         registerPlugin(DeviceAccessPlugin.class);
         super.onCreate(savedInstanceState);
+        ViewCompat.setOnApplyWindowInsetsListener(getBridge().getWebView(), (view, insets) -> {
+            WindowInsetsCompat bars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            String script = "document.documentElement.style.setProperty('--android-safe-top','" + bars.top
+                    + "px');document.documentElement.style.setProperty('--android-safe-bottom','" + bars.bottom
+                    + "px');document.documentElement.style.setProperty('--android-safe-left','" + bars.left
+                    + "px');document.documentElement.style.setProperty('--android-safe-right','" + bars.right + "px');";
+            ((WebView) view).evaluateJavascript(script, null);
+            return insets;
+        });
+        ViewCompat.requestApplyInsets(getBridge().getWebView());
     }
 }
